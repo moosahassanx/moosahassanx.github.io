b5a5629b0a7bdc51f8b9c3a6a76e70aff0d97a6e
Moosa <moosa7021@gmail.com>
2022-04-06 15:15:04 +1000

    initial deployment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing GitHub Pages in React
         </a>
       </header>
     </div>
