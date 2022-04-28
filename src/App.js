4d7ccd1b3fe38d1e23f75de5fa7b02864d6c6798
Moosa Hassan <moosa7021@gmail.com>
2022-04-28 15:20:54 +1000

    code cleanup + resume downloader

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import Page from './pages/index';
 import * as React from 'react';
 import Navbar from './components/navbar';
+import { 
+  HashRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Works from './pages/projects';
+import University from './pages/university';
+import { Container, Box, Heading } from "@chakra-ui/react";
 
 // hook function
 const Main = () => {
   return(
     <div>
-        {/* present index page only to emulate devaslife code */}
-        <Navbar />
+        <HashRouter>
+          {/* navigation includes router */}
+          <Navbar />
+
+          {/* this content will always display */}
+          <Container>
+            <Box borderRadius="lg" mb={6} p={3} textAlign="center" bg={'blue'}>
+              This content displays across all pages.
+            </Box>
+          </Container>
+
+          {/* display content based on router */}
+          <div className="content">
+                <Routes>
+                    <Route exact path="/" element={<Page/>} />
+                    <Route exact path="/works" element={<Works/>} />
+                    <Route path="/uni" element={<University/>} />
+                </Routes>
+            </div>
+        </HashRouter>
+        
     </div>
   )
 }
 
-// react initial starting point
+// redirect to hook
 function App() {
-  // redirect to hook function
   return (
     <Main />
   )
